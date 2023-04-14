6058b17872fbcd6409bdac3ca0c9d21ccab88418
Yur <sergiy.yur@decimadigital.com>
2023-04-14 16:34:16 +0300

    Update action

--- Test/Integration/etc/install-config-mysql.php
+++ Test/Integration/etc/install-config-mysql.php
@@ -1,26 +1,23 @@
 <?php
 
-print_r($_ENV);
-print_r($_REQUEST);
-print_r($_SERVER);
 return [
-    'db-host' => $_ENV['DB_HOST'],
-    'db-user' => $_ENV['DB_USER'],
-    'db-password' => $_ENV['DB_PASSWORD'],
-    'db-name' => $_ENV['DB_NAME'],
+    'db-host' => $_SERVER['INPUT_DB_HOST'],
+    'db-user' => $_SERVER['INPUT_DB_USER'],
+    'db-password' => $_SERVER['INPUT_DB_PASSWORD'],
+    'db-name' => $_SERVER['INPUT_DB_NAME'],
     'db-prefix' => '',
     'backend-frontname' => 'backend',
     'search-engine' => 'opensearch',
-    'opensearch-host' => $_ENV['OPENSEARCH_HOST'],
-    'opensearch-port' => $_ENV['OPENSEARCH_PORT'],
+    'opensearch-host' => $_SERVER['INPUT_OPENSEARCH_HOST'],
+    'opensearch-port' => $_SERVER['INPUT_OPENSEARCH_PORT'],
     'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
     'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
     'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
     'admin-firstname' => \Magento\TestFramework\Bootstrap::ADMIN_FIRSTNAME,
     'admin-lastname' => \Magento\TestFramework\Bootstrap::ADMIN_LASTNAME,
-    'amqp-host' => $_ENV['AMQP_HOST'],
-    'amqp-port' => $_ENV['AMQP_PORT'],
-    'amqp-user' => $_ENV['AMQP_USER'],
-    'amqp-password' => $_ENV['AMQP_PASSWORD'],
+    'amqp-host' => $_SERVER['INPUT_AMQP_HOST'],
+    'amqp-port' => $_SERVER['INPUT_AMQP_PORT'],
+    'amqp-user' => $_SERVER['INPUT_AMQP_USER'],
+    'amqp-password' => $_SERVER['INPUT_AMQP_PASSWORD'],
     'consumers-wait-for-messages' => '0',
 ];
